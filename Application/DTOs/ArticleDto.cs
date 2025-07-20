f6a827ed8c50fc825bec850db439d9810cc0c9bc
Shohboz Xurramov <shohboztuit@gmail.com>
2025-07-21 00:55:57 +0500

    created Entities

--- Application/DTOs/ArticleDto.cs
+++ Application/DTOs/ArticleDto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Entities
+namespace Application.DTOs
 {
-    internal class Artecle
+    public class ArticleDto
     {
     }
 }
---
+++ Domain/Entities/Member .cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+public class Member
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Degree { get; set; }
+    public string Info { get; set; }
+    public string PhotoUrl { get; set; }
+}
